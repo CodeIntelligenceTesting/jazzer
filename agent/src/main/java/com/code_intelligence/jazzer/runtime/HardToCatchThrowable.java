2dc537a6b43d75baf960989b5adb97b299037512
Fabian Meumertzheim <meumertzheim@code-intelligence.com>
2021-03-25 16:58:52 +0100

    Add Jazzer API method for reporting findings from hooks
    The method Jazzer.reportFindingFromHook can be used to reliably report a
    finding from a hook, even if the hook is called within an overly generic
    exception handler.
    
    In order to make the fuzz target return quickly, a hard to catch error
    is thrown after the finding has been registered via the global finding
    variable. This is not guaranteed to terminate the execution of the fuzz
    target right away, but the global finding variable ensures that the
    finding will be reported eventually (unless the fuzz target never
    returns).

---
+++ agent/src/main/java/com/code_intelligence/jazzer/runtime/HardToCatchThrowable.java
@@ -0,0 +1,82 @@
+// Copyright 2021 Code Intelligence GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.code_intelligence.jazzer.runtime;
+
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+/**
+ * An Error that rethrows itself when any of its getters is invoked.
+ */
+public class HardToCatchThrowable extends Error {
+  public HardToCatchThrowable() {
+    super();
+  }
+
+  @Override
+  public String getMessage() {
+    throw this;
+  }
+
+  @Override
+  public String getLocalizedMessage() {
+    throw this;
+  }
+
+  @Override
+  public synchronized Throwable initCause(Throwable cause) {
+    throw this;
+  }
+
+  @Override
+  public String toString() {
+    throw this;
+  }
+
+  @Override
+  public void printStackTrace() {
+    throw this;
+  }
+
+  @Override
+  public void printStackTrace(PrintStream s) {
+    throw this;
+  }
+
+  @Override
+  public void printStackTrace(PrintWriter s) {
+    throw this;
+  }
+
+  @Override
+  public StackTraceElement[] getStackTrace() {
+    throw this;
+  }
+
+  @Override
+  public int hashCode() {
+    throw this;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    throw this;
+  }
+
+  @Override
+  public Object clone() {
+    throw this;
+  }
+}
