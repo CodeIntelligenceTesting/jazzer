818a5033a26d45c2ba4114efc9f4bff4b222edc1
Fabian Meumertzheim <fabian@meumertzhe.im>
2021-10-16 07:43:40 +0200

    Return native path to temp file in driver
    The path returned by generic_string() does not use backslashes as path
    segment separators on Windows.

--- driver/libfuzzer_driver.cpp
+++ driver/libfuzzer_driver.cpp
@@ -73,7 +73,7 @@ std::string GetNewTempFilePath() {
   if (std::filesystem::exists(temp_path))
     throw std::runtime_error("Random temp file path exists: " +
                              temp_path.string());
-  return temp_path.generic_string();
+  return temp_path.string();
 }
 }  // namespace
 
