1ef9a078f196c997a4ec3bdd8378226804438b2b
Norbert Schneider <norbert.schneider@code-intelligence.com>
2022-01-11 13:43:17 +0100

    Use pre-generated input file in ExampleLongStringFuzzer test
    dd is not available on all test platforms / environments in the CI pipeline. In
    this case the input file is small enough to be checked in.

--- bazel/FuzzTargetTestWrapper.java
+++ bazel/FuzzTargetTestWrapper.java
@@ -24,7 +24,6 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import javax.tools.JavaCompiler;
