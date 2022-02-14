914562753d4ff4932c55de9376395c105d24a8c6
Norbert Schneider <norbert.schneider@code-intelligence.com>
2022-02-14 15:37:24 +0100

    Add OS command injection bug detector
    This bug detector checks if fuzzer provided data reaches os command execution
    code.

---
+++ sanitizers/src/test/java/com/example/OsCommandInjectionProcessBuilder.java
@@ -0,0 +1,34 @@
+// Copyright 2021 Code Intelligence GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.example;
+
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class OsCommandInjectionProcessBuilder {
+  public static void fuzzerTestOneInput(FuzzedDataProvider data) {
+    String input = data.consumeRemainingAsAsciiString();
+    try {
+      Process process = new ProcessBuilder(input).start();
+      // This should be way faster, but we have to wait until the call is done
+      if (!process.waitFor(10, TimeUnit.MILLISECONDS)) {
+        process.destroyForcibly();
+      }
+    } catch (Exception ignored) {
+      // Ignore execution and setup exceptions
+    }
+  }
+}
