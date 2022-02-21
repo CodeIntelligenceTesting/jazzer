23b08b6b64155621386967e089021fa75983f59c
Fabian Meumertzheim <fabian@meumertzhe.im>
2022-02-21 09:58:40 +0100

    Add benchmarks for fuzzer callbacks
    The parts of the benchmark that require a JDK supporting Project Panama
    are commented out as there is no good way to enable them only if the
    current JDK supports them.

---
+++ agent/src/jmh/java/com/code_intelligence/jazzer/runtime/FuzzerCallbacks.java
@@ -0,0 +1,29 @@
+// Copyright 2022 Code Intelligence GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.code_intelligence.jazzer.runtime;
+
+import com.github.fmeum.rules_jni.RulesJni;
+
+public final class FuzzerCallbacks {
+  static {
+    RulesJni.loadLibrary("fuzzer_callbacks", FuzzerCallbacks.class);
+  }
+
+  static native void traceCmpInt(int arg1, int arg2, int pc);
+  static native void traceSwitch(long val, long[] cases, int pc);
+
+  static native void traceMemcmp(byte[] b1, byte[] b2, int result, int pc);
+  static native void traceStrstr(String s1, String s2, int pc);
+}
---
+++ agent/src/jmh/java/com/code_intelligence/jazzer/runtime/FuzzerCallbacksWithPc.java
@@ -0,0 +1,31 @@
+// Copyright 2022 Code Intelligence GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.code_intelligence.jazzer.runtime;
+
+import com.github.fmeum.rules_jni.RulesJni;
+
+/**
+ * Unoptimized implementation of the libFuzzer callbacks that use the trampoline construction to
+ * inject fake PCs.
+ */
+public final class FuzzerCallbacksWithPc {
+  static {
+    RulesJni.loadLibrary("fuzzer_callbacks", FuzzerCallbacksWithPc.class);
+  }
+
+  static native void traceCmpInt(int arg1, int arg2, int pc);
+
+  static native void traceSwitch(long val, long[] cases, int pc);
+}
