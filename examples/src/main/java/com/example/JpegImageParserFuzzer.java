0dbda45a4f6d25ceaf5b168da5d96d5abf410725
Fabian Meumertzheim <fabian@meumertzhe.im>
2022-03-01 10:45:35 +0100

    Verify the precise exception reproduced by a reproducer
    New and existing verifications performed by the FuzzTargetTestWrapper
    are now only done if JAZZER_CI is set. This keeps the tests that do
    double duty as examples clean when executed by users, but still
    maximizes their test coverage. Developers can specify --config=ci to
    benefit from these extended checks.

--- examples/src/main/java/com/example/JpegImageParserFuzzer.java
+++ examples/src/main/java/com/example/JpegImageParserFuzzer.java
@@ -22,28 +22,6 @@ import org.apache.commons.imaging.formats.jpeg.JpegImageParser;
 
 // Found https://issues.apache.org/jira/browse/IMAGING-275.
 public class JpegImageParserFuzzer {
-  public static void fuzzerInitialize() {
-    String foo = System.getProperty("foo");
-    String bar = System.getProperty("bar");
-    String baz = System.getProperty("baz");
-    // Only used to verify that arguments are correctly passed down to child processes.
-    if (foo == null || bar == null || baz == null || !foo.equals("foo")
-        || !(bar.equals("b;ar") || bar.equals("b:ar")) || !baz.equals("baz")) {
-      // Exit the process with an exit code different from that for a finding.
-      System.err.println("ERROR: Did not correctly pass all jvm_args to child process.");
-      System.err.printf("foo: %s%nbar: %s%nbaz: %s%n", foo, bar, baz);
-      System.exit(3);
-    }
-    // Only used to verify that Jazzer honors the JAVA_OPTS env var.
-    String javaOpts = System.getProperty("java_opts");
-    if (javaOpts == null || !javaOpts.equals("1")) {
-      // Exit the process with an exit code different from that for a finding.
-      System.err.println("ERROR: Did not honor JAVA_OPTS.");
-      System.err.printf("java_opts: %s%n", javaOpts);
-      System.exit(4);
-    }
-  }
-
   public static void fuzzerTestOneInput(byte[] input) {
     try {
       new JpegImageParser().getBufferedImage(new ByteSourceArray(input), new HashMap<>());
