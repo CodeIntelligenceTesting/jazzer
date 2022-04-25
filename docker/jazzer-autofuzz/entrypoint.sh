ab21bdc5f0b8788381ded9b8e6398d4de9b0c172
Fabian Meumertzheim <meumertzheim@code-intelligence.com>
2022-04-25 17:41:20 +0200

    Simplify jazzer-autofuzz entrypoint
    Setting the prefixes explicitly is not necessary due to the WORKDIR
    directive. Dropping them has the additional benefit that printed paths
    will resolve on the host when mounting $(pwd).

--- docker/jazzer-autofuzz/entrypoint.sh
+++ docker/jazzer-autofuzz/entrypoint.sh
@@ -17,8 +17,6 @@ set -e
 
 CP="$(/app/coursier.jar fetch --classpath "$1")"
 /app/jazzer_driver \
-  -artifact_prefix=/fuzzing/ \
-  --reproducer_path=/fuzzing \
   --cp="$CP" \
   --autofuzz="$2" \
   "${@:3}"
