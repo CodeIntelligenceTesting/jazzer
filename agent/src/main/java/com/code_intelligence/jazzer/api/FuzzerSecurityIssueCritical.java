c08ab3d00d95f08a3539f9bda522f31ad3f6426a
Fabian Meumertzheim <fabian@meumertzhe.im>
2022-08-11 10:09:34 +0200

    api: Fix paragraphs and links in Javadoc
    <p> is required to begin a new paragraph, empty lines aren't enough.

--- agent/src/main/java/com/code_intelligence/jazzer/api/FuzzerSecurityIssueCritical.java
+++ agent/src/main/java/com/code_intelligence/jazzer/api/FuzzerSecurityIssueCritical.java
@@ -17,7 +17,7 @@ package com.code_intelligence.jazzer.api;
 /**
  * Thrown to indicate that a fuzz target has detected a critical severity security issue rather than
  * a normal bug.
- *
+ * <p>
  * There is only a semantical but no functional difference between throwing exceptions of this type
  * or any other. However, automated fuzzing platforms can use the extra information to handle the
  * detected issues appropriately.
