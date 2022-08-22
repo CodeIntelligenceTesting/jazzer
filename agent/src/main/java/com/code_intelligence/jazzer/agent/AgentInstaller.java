a5ff1921625791b8da6853f81bbad78dfc080185
Fabian Meumertzheim <meumertzheim@code-intelligence.com>
2022-08-22 09:20:05 +0200

    all: Only append hooks to the bootstrap classpath
    Previously, we appended the entire Jazzer agent with all its
    dependencies to the bootstrap classpath, which resulted in a number of
    classes being loaded both in the system and bootstrap class loader when
    the agent wasn't initialized right after the JVM had been started.
    
    With this commit, the agent JAR bundles a separate bootstrap JAR that
    only includes the classes that strictly must be available on the
    bootstrap class loader's classpath: Classes defining callbacks called by
    our instrumentation as well as the native driver library which these
    callbacks call into.
    
    The new AgentInstaller class takes care of extracting and appending this
    JAR to the bootstrap search path. For the sake of consistency, it does
    this even if --nohooks is specified, but doesn't enable the agent in
    that case.

---
+++ agent/src/main/java/com/code_intelligence/jazzer/agent/AgentInstaller.java
@@ -0,0 +1,85 @@
+// Copyright 2022 Code Intelligence GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.code_intelligence.jazzer.agent;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.instrument.Instrumentation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.nio.file.Files;
+import java.nio.file.StandardCopyOption;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.jar.JarFile;
+import net.bytebuddy.agent.ByteBuddyAgent;
+
+public class AgentInstaller {
+  private static final String BOOTSTRAP_JAR =
+      "/com/code_intelligence/jazzer/runtime/jazzer_bootstrap.jar";
+  private static final AtomicBoolean hasBeenInstalled = new AtomicBoolean();
+  private static File bootstrapJar;
+
+  /**
+   * Appends the parts of Jazzer that have to be visible to all classes, including those in the Java
+   * standard library, to the bootstrap class loader path. Additionally, if enableAgent is true,
+   * also enables the Jazzer agent that instruments classes for fuzzing.
+   */
+  public static void install(boolean enableAgent) {
+    // Only install the agent once.
+    if (!hasBeenInstalled.compareAndSet(false, true)) {
+      return;
+    }
+    Instrumentation instrumentation = ByteBuddyAgent.install();
+    bootstrapJar = extractBootstrapJar();
+    try {
+      instrumentation.appendToBootstrapClassLoaderSearch(new JarFile(bootstrapJar));
+    } catch (IOException e) {
+      throw new IllegalStateException(
+          "Failed to append Jazzer agent bootstrap jar to bootstrap class loader search", e);
+    }
+    if (!enableAgent) {
+      return;
+    }
+    try {
+      Class<?> agent = Class.forName("com.code_intelligence.jazzer.agent.Agent");
+      Method premain = agent.getMethod("premain", String.class, Instrumentation.class);
+      premain.invoke(null, null, instrumentation);
+    } catch (ClassNotFoundException | InvocationTargetException | NoSuchMethodException
+        | IllegalAccessException e) {
+      throw new IllegalStateException("Failed to run Agent.premain", e);
+    }
+  }
+
+  public static void deleteTemporaryFiles() {
+    if (bootstrapJar != null) {
+      bootstrapJar.delete();
+    }
+  }
+
+  private static File extractBootstrapJar() {
+    try (InputStream bootstrapJarStream = AgentInstaller.class.getResourceAsStream(BOOTSTRAP_JAR)) {
+      if (bootstrapJarStream == null) {
+        throw new IllegalStateException("Failed to find Jazzer agent boostrap jar in resources");
+      }
+      File bootstrapJar = Files.createTempFile("jazzer-agent-", ".jar").toFile();
+      bootstrapJar.deleteOnExit();
+      Files.copy(bootstrapJarStream, bootstrapJar.toPath(), StandardCopyOption.REPLACE_EXISTING);
+      return bootstrapJar;
+    } catch (IOException e) {
+      throw new IllegalStateException("Failed to extract Jazzer agent bootstrap jar", e);
+    }
+  }
+}
---
+++ agent/src/main/java/com/code_intelligence/jazzer/runtime/FuzzTargetRunnerNatives.java
@@ -0,0 +1,40 @@
+// Copyright 2022 Code Intelligence GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.code_intelligence.jazzer.runtime;
+
+import com.github.fmeum.rules_jni.RulesJni;
+
+/**
+ * The native functions used by FuzzTargetRunner.
+ *
+ * <p>This class has to be loaded by the bootstrap class loader since the native library it loads
+ * links in libFuzzer and the Java hooks, which have to be on the bootstrap path so that they are
+ * seen by Java standard library classes, need to be able to call native libFuzzer callbacks.
+ */
+public class FuzzTargetRunnerNatives {
+  static {
+    if (FuzzTargetRunnerNatives.class.getClassLoader() != null) {
+      throw new IllegalStateException(
+          "FuzzTargetRunnerNatives must be loaded in the boostrap loader");
+    }
+    RulesJni.loadLibrary("jazzer_driver", "/com/code_intelligence/jazzer/driver");
+  }
+
+  public static native int startLibFuzzer(byte[][] args, Class<?> runner);
+
+  public static native void printCrashingInput();
+
+  public static native void _Exit(int exitCode);
+}
