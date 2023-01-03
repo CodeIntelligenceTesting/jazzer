5bb6d4bb7da2acc2303cc824cac37798ee6f45c7
Khaled Yakdan <yakdan@code-intelligence.com>
2023-01-03 09:29:00 +0100

    sanitizers: refactor the expression language injection test

--- sanitizers/src/test/java/com/example/el/InsecureEmailValidator.java
+++ sanitizers/src/test/java/com/example/el/InsecureEmailValidator.java
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.example;
+package com.example.el;
 
 import static java.lang.String.format;
 
