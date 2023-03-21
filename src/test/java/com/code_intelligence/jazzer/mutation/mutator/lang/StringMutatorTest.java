ac2adfcd7a6a46765854a85e1ad4e795a22d001a
Brian Lewis <keller@csh.rit.edu>
2023-03-21 16:34:30 +0100

    Specify utf8 in multibyte character test

--- src/test/java/com/code_intelligence/jazzer/mutation/mutator/lang/StringMutatorTest.java
+++ src/test/java/com/code_intelligence/jazzer/mutation/mutator/lang/StringMutatorTest.java
@@ -178,7 +178,7 @@ class StringMutatorTest {
     assertThat(mutator.toString()).isEqualTo("String");
 
     String s;
-    try (MockPseudoRandom prng = mockPseudoRandom(10, "foobarÖÖ".getBytes())) {
+    try (MockPseudoRandom prng = mockPseudoRandom(10, "foobarÖÖ".getBytes(StandardCharsets.UTF_8))) {
       s = mutator.init(prng);
     }
     assertThat(s).hasLength(8);
