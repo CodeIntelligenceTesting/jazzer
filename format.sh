b1ab3cf48a212709c7a7dbb7a17cbc45fe9d43aa
Fabian Meumertzheim <meumertzheim@code-intelligence.com>
2023-03-30 12:32:32 +0200

    bazel: Run buildifier with Bazel
    Also converts the buildifier check to a regular Bazel test.

--- format.sh
+++ format.sh
@@ -16,12 +16,14 @@
 
 set -euo pipefail
 
+THIS_DIR="$(pwd -P)"
+
 # C++ & Java
 find -name '*.cpp' -o -name '*.c' -o -name '*.h' -o -name '*.java' | xargs clang-format-14 -i
 
-# Kotlin
-# No need to run in CI as the ktlint_tests will fail if the formatting is wrong.
+# No need to run in CI as these formatters have corresponding Bazel tests.
 if [[ "${CI:-0}" == 0 ]]; then
+    # Kotlin
     # Check which ktlint_tests failed and run the corresponding fix targets. This is much faster than
     # running all ktlint_fix targets when e.g. only a few or no .kt files changed.
     # shellcheck disable=SC2046
@@ -29,11 +31,10 @@ if [[ "${CI:-0}" == 0 ]]; then
     if [[ -n "${TARGETS_TO_RUN}" ]]; then
         echo "$TARGETS_TO_RUN" | xargs -n 1 bazel run --config=quiet
     fi
-fi
 
-# BUILD files
-# go install github.com/bazelbuild/buildtools/buildifier@latest
-buildifier -r .
+    # BUILD files
+    bazel run --config=quiet //:buildifier -- -r "$THIS_DIR"
+fi
 
 # Licence headers
 # go install github.com/google/addlicense@latest
