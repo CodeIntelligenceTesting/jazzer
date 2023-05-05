5a9c9c0cb4d331f2367d88629a669e7b208940c8
Fabian Meumertzheim <meumertzheim@code-intelligence.com>
2023-05-05 08:50:01 +0200

    junit: Use marker arguments to signal start of fuzzing
    This is intended to be a pure refactoring that enables us to allow
    arbitrary `ArgumentsProvider`s to provide arguments for fuzz tests that
    are picked up in fuzzing mode.

--- src/test/java/com/code_intelligence/jazzer/junit/UtilsTest.java
+++ src/test/java/com/code_intelligence/jazzer/junit/UtilsTest.java
@@ -15,18 +15,87 @@
 package com.code_intelligence.jazzer.junit;
 
 import static com.code_intelligence.jazzer.junit.Utils.durationStringToSeconds;
+import static com.code_intelligence.jazzer.junit.Utils.getMarkedArguments;
+import static com.code_intelligence.jazzer.junit.Utils.getMarkedInstance;
+import static com.code_intelligence.jazzer.junit.Utils.isMarkedInstance;
+import static com.code_intelligence.jazzer.junit.Utils.isMarkedInvocation;
 import static com.google.common.truth.Truth.assertThat;
+import static java.util.Arrays.stream;
+import static java.util.Collections.singletonList;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
 
-import org.junit.Test;
+import java.lang.reflect.Method;
+import java.util.AbstractList;
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.api.extension.InvocationInterceptor;
+import org.junit.jupiter.api.extension.ReflectiveInvocationContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-public class UtilsTest {
+public class UtilsTest implements InvocationInterceptor {
   @Test
-  public void testDurationStringToSeconds() {
+  void testDurationStringToSeconds() {
     assertThat(durationStringToSeconds("1m")).isEqualTo(60);
     assertThat(durationStringToSeconds("1min")).isEqualTo(60);
     assertThat(durationStringToSeconds("1h")).isEqualTo(60 * 60);
     assertThat(durationStringToSeconds("1h   2m 30s")).isEqualTo(60 * 60 + 2 * 60 + 30);
     assertThat(durationStringToSeconds("1hr2min30sec")).isEqualTo(60 * 60 + 2 * 60 + 30);
     assertThat(durationStringToSeconds("1h2m30s")).isEqualTo(60 * 60 + 2 * 60 + 30);
+  }
+
+  @ValueSource(classes = {int.class, Class.class, Object.class, String.class, HashMap.class,
+                   Map.class, int[].class, int[][].class, AbstractMap.class, AbstractList.class})
+  @ParameterizedTest
+  void
+  testMarkedInstances(Class<?> clazz) {
+    Object instance = getMarkedInstance(clazz);
+    if (clazz == int.class) {
+      assertThat(instance).isInstanceOf(Integer.class);
+    } else {
+      assertThat(instance).isInstanceOf(clazz);
+    }
+    assertThat(isMarkedInstance(instance)).isTrue();
+    assertThat(getMarkedInstance(clazz)).isSameInstanceAs(instance);
+  }
+
+  static Stream<Arguments> testWithMarkedNamedParametersSource() {
+    Method testMethod =
+        stream(UtilsTest.class.getDeclaredMethods())
+            .filter(method -> method.getName().equals("testWithMarkedNamedParameters"))
+            .findFirst()
+            .get();
+    return Stream.of(
+        arguments("foo", 0, new HashMap<>(), singletonList(5), UtilsTest.class, new int[] {1}),
+        getMarkedArguments(testMethod, "some name"),
+        arguments("baz", 1, new LinkedHashMap<>(), Arrays.asList(5, 7), String.class, new int[0]),
+        getMarkedArguments(testMethod, "some other name"));
+  }
+
+  @MethodSource("testWithMarkedNamedParametersSource")
+  @ExtendWith(UtilsTest.class)
+  @ParameterizedTest
+  void testWithMarkedNamedParameters(String str, int num, AbstractMap<String, String> map,
+      List<Integer> list, Class<?> clazz, int[] array) {}
+
+  boolean argumentsExpectedToBeMarked = false;
+
+  @Override
+  public void interceptTestTemplateMethod(Invocation<Void> invocation,
+      ReflectiveInvocationContext<Method> invocationContext, ExtensionContext extensionContext)
+      throws Throwable {
+    assertThat(isMarkedInvocation(invocationContext)).isEqualTo(argumentsExpectedToBeMarked);
+    argumentsExpectedToBeMarked = !argumentsExpectedToBeMarked;
+    invocation.proceed();
   }
 }
