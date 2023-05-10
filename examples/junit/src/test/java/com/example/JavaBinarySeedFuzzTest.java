f80c2171171a2c8f1c4c0ac2464ec8c185c0abae
Fabian Meumertzheim <meumertzheim@code-intelligence.com>
2023-05-10 08:34:15 +0200

    junit: Record parameterized test arguments as seeds
    The seeds are serialized to files in a temporary directory that is
    passed to libFuzzer as an additional seed directory.

---
+++ examples/junit/src/test/java/com/example/JavaBinarySeedFuzzTest.java
@@ -0,0 +1,66 @@
+/*
+ * Copyright 2023 Code Intelligence GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
+
+import com.code_intelligence.jazzer.junit.FuzzTest;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import org.junit.jupiter.params.converter.ArgumentConversionException;
+import org.junit.jupiter.params.converter.ConvertWith;
+import org.junit.jupiter.params.converter.SimpleArgumentConverter;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class JavaBinarySeedFuzzTest {
+  // Generated via:
+  // printf 'tH15_1S-4_53Cr3T.fl4G' | openssl dgst -binary -sha256 | openssl base64 -A
+  // Luckily the fuzzer can't read comments ;-)
+  private static final byte[] FLAG_SHA256 =
+      Base64.getDecoder().decode("q0vPdz5oeJIW3k2U4VJ+aWDufzzZbKAcevc9cNoUTSM=");
+
+  static class Utf8BytesConverter extends SimpleArgumentConverter {
+    @Override
+    protected Object convert(Object source, Class<?> targetType)
+        throws ArgumentConversionException {
+      assertEquals(byte[].class, targetType);
+      assertTrue(source instanceof byte[] || source instanceof String);
+      if (source instanceof byte[]) {
+        return source;
+      }
+      return ((String) source).getBytes(UTF_8);
+    }
+  }
+
+  @ValueSource(strings = {"red herring", "tH15_1S-4_53Cr3T.fl4Ga"})
+  @FuzzTest
+  void fuzzTheFlag(@ConvertWith(Utf8BytesConverter.class) byte[] bytes)
+      throws NoSuchAlgorithmException {
+    assumeTrue(bytes.length > 0);
+    MessageDigest digest = MessageDigest.getInstance("SHA-256");
+    digest.update(bytes, 0, bytes.length - 1);
+    byte[] hash = digest.digest();
+    byte secret = bytes[bytes.length - 1];
+    if (MessageDigest.isEqual(hash, FLAG_SHA256) && secret == 's') {
+      throw new Error("Fl4g 4nd s3cr3et f0und!");
+    }
+  }
+}
---
+++ examples/junit/src/test/java/com/example/JavaSeedFuzzTest.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright 2023 Code Intelligence GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+import com.code_intelligence.jazzer.junit.FuzzTest;
+import com.code_intelligence.jazzer.mutation.annotation.NotNull;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import java.util.List;
+import java.util.stream.Stream;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+class JavaSeedFuzzTest {
+  // Generated via:
+  // printf 'tH15_1S-4_53Cr3T.fl4G' | openssl dgst -binary -sha256 | openssl base64 -A
+  // Luckily the fuzzer can't read comments ;-)
+  private static final byte[] FLAG_SHA256 =
+      Base64.getDecoder().decode("q0vPdz5oeJIW3k2U4VJ+aWDufzzZbKAcevc9cNoUTSM=");
+
+  static Stream<Arguments> fuzzTheFlag() {
+    return Stream.of(arguments(asList("red", "herring"), 0),
+        // This argument passes the hash check, but does not trigger the finding right away. This
+        // is meant to verify that the seed ends up in the corpus, serving as the base for future
+        // mutations rather than just being executed once.
+        arguments(asList("tH15_1S", "-4_53Cr3T", ".fl4G"), 42));
+  }
+
+  @MethodSource
+  @FuzzTest
+  void fuzzTheFlag(@NotNull List<@NotNull String> flagParts, int secret)
+      throws NoSuchAlgorithmException {
+    byte[] hash = MessageDigest.getInstance("SHA-256").digest(
+        String.join("", flagParts).getBytes(StandardCharsets.UTF_8));
+    if (MessageDigest.isEqual(hash, FLAG_SHA256) && secret == 1337) {
+      throw new Error("Fl4g 4nd s3cr3et f0und!");
+    }
+  }
+}
