10954df1df3c4e737b33dcee7142c149c08bfdb0
Norbert Schneider <norbert.schneider@code-intelligence.com>
2023-06-09 09:48:01 +0200

    Honor explicitly stated corpus directory
    The JUnit integration always created corpus files in .cifuzz-corpus
    subdirectories, even if an explicit corpus directory was stated via
    command line. Now explicit corpus parameters are honored.

---
+++ examples/junit/src/test/java/com/example/CorpusDirectoryFuzzTest.java
@@ -0,0 +1,45 @@
+// Copyright 2023 Code Intelligence GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.example;
+
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+import com.code_intelligence.jazzer.api.FuzzerSecurityIssueMedium;
+import com.code_intelligence.jazzer.junit.FuzzTest;
+
+public class CorpusDirectoryFuzzTest {
+  private static int invocations = 0;
+
+  @FuzzTest(maxDuration = "5s")
+  public void corpusDirectoryFuzz(FuzzedDataProvider data) {
+    // Throw on the third invocation to generate corpus entries.
+    if (data.remainingBytes() == 0) {
+      return;
+    }
+    // Add a few branch statements to generate different coverage.
+    switch (invocations) {
+      case 0:
+        invocations++;
+        break;
+      case 1:
+        invocations++;
+        break;
+      case 2:
+        invocations++;
+        break;
+      case 3:
+        throw new FuzzerSecurityIssueMedium();
+    }
+  }
+}
