630f4aba0bd796134fbe9fc34eac6b9a91e4f9f9
Robert Czechowski <robert@code-intelligence.com>
2023-08-01 18:01:07 +0200

    Clojure fuzzing: Add test for numerical comparison functions

---
+++ sanitizers/src/test/java/com/example/ClojureTests.java
@@ -0,0 +1,41 @@
+// Copyright 2022 Code Intelligence GmbH
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.example;
+
+import clojure.lang.Numbers;
+import com.code_intelligence.jazzer.api.FuzzedDataProvider;
+import java.sql.Connection;
+import java.sql.SQLException;
+import org.h2.jdbcx.JdbcDataSource;
+
+class ClojureFindingException extends Exception {}
+
+public class ClojureTests {
+  static void insecureCrashOnCertainNumbers(Long lnumber, Integer inumber, Long divisor)
+      throws Exception {
+    if (clojure.lang.Numbers.lt(lnumber, (Long) (long) 218461)
+        && clojure.lang.Numbers.lt((Long) (long) 218459, lnumber)) {
+      if (clojure.lang.Numbers.lt(inumber, (Integer) 318461)
+          && clojure.lang.Numbers.lt((Integer) 318459, inumber)) {
+        // This will throw a java.lang.ArithmeticException when divisor becomes zero
+        clojure.lang.Numbers.divide((Integer) 318461, divisor);
+      }
+    }
+  }
+
+  public static void fuzzerTestOneInput(FuzzedDataProvider data) throws Exception {
+    insecureCrashOnCertainNumbers(data.consumeLong(), data.consumeInt(), data.consumeLong());
+  }
+}
