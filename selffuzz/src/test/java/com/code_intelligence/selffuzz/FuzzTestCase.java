b03bff694150613898d0a4bd0ff0b34c6577a843
Brian Lewis <keller@csh.rit.edu>
2023-08-08 15:12:13 +0200

    Make the ignored error more specific

--- selffuzz/src/test/java/com/code_intelligence/selffuzz/FuzzTestCase.java
+++ selffuzz/src/test/java/com/code_intelligence/selffuzz/FuzzTestCase.java
@@ -17,25 +17,21 @@
 package com.code_intelligence.selffuzz;
 
 import com.code_intelligence.jazzer.junit.FuzzTest;
-import com.code_intelligence.selffuzz.jazzer.mutation.annotation.proto.WithDefaultInstance;
-import com.code_intelligence.selffuzz.jazzer.mutation.mutator.proto.ProtoMutators;
 import com.code_intelligence.selffuzz.jazzer.mutation.annotation.NotNull;
+import com.code_intelligence.selffuzz.jazzer.mutation.annotation.proto.WithDefaultInstance;
 import com.code_intelligence.selffuzz.jazzer.mutation.api.SerializingMutator;
 import com.code_intelligence.selffuzz.jazzer.mutation.mutator.lang.LangMutators;
+import com.code_intelligence.selffuzz.jazzer.mutation.mutator.proto.ProtoMutators;
 import com.code_intelligence.selffuzz.jazzer.mutation.support.TypeHolder;
-import com.google.protobuf.DescriptorProtos;
 import com.google.protobuf.DescriptorProtos.DescriptorProto;
 import com.google.protobuf.DescriptorProtos.FileDescriptorProto;
 import com.google.protobuf.Descriptors;
-import com.google.protobuf.DynamicMessage;
 import com.google.protobuf.Descriptors.FileDescriptor;
-import com.google.protobuf.Message;
-
+import com.google.protobuf.DynamicMessage;
 import java.io.DataInputStream;
 import java.io.EOFException;
 import java.io.IOException;
-import java.lang.reflect.AnnotatedParameterizedType;
-import java.lang.reflect.AnnotatedType;
+import java.lang.reflect.InvocationTargetException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -62,38 +58,45 @@ class FuzzTestCase {
   // https://protobuf.dev/reference/protobuf/proto3-spec/#identifiers
   private static Pattern protoNamePattern = Pattern.compile("^[a-zA-Z][a-zA-Z._]$");
 
+  @SuppressWarnings({"unchecked", "unused"})
   @FuzzTest
-  void dynamicMessageFuzzTest(@NotNull DescriptorProto messageType, byte @NotNull [] bytes) {
-    System.out.printf("messageType: %s%n", messageType);
+  void dynamicMessageFuzzTest(@NotNull DescriptorProto messageType, byte @NotNull[] bytes) {
     if (messageType == null) {
       return;
     }
 
-    /*
     protoName = messageType.getName();
     Matcher matcher = protoNamePattern.matcher(protoName);
-    System.out.println(protoName);
     if (!matcher.find()) {
       return;
     }
-     */
     file = FileDescriptorProto.newBuilder()
-            .setName("my_protos.proto")
-            .addMessageType(messageType)
-            .build();
+               .setName("my_protos.proto")
+               .addMessageType(messageType)
+               .build();
 
     try (DataInputStream stream = Helpers.infiniteByteStream(bytes)) {
-
       SerializingMutator<DynamicMessage> mutator =
-              (SerializingMutator<DynamicMessage>) ProtoMutators.newFactory()
-                      .createOrThrow(
-                              new TypeHolder<@WithDefaultInstance("com.code_intelligence.selffuzz.FuzzTestCase#getDefaultInstance") DynamicMessage>() {}.annotatedType());
+          (SerializingMutator<DynamicMessage>) ProtoMutators.newFactory().createOrThrow(
+              new TypeHolder<@WithDefaultInstance(
+                  "com.code_intelligence.selffuzz.FuzzTestCase#getDefaultInstance")
+                  DynamicMessage>() {
+              }.annotatedType());
+
       DynamicMessage out = mutator.read(stream);
-      System.out.println(protoName);
     } catch (IllegalArgumentException e) {
-      // an invalid proto descriptor will throw a DescriptorValidationException below but by the time it gets here
-      // it'll be an IllegalArgumentException
-      return;
+      // an invalid proto descriptor will throw a DescriptorValidationException below but by the
+      // time it gets here it'll be wrapped in a couple layers of other exceptions. We peel it apart
+      // to make sure we only ignore DescriptorValidationExceptions and throw the error if it's
+      // anything else
+      if (e.getCause() instanceof InvocationTargetException) {
+        InvocationTargetException invocationException = (InvocationTargetException) e.getCause();
+        if (invocationException.getTargetException().getCause()
+                instanceof Descriptors.DescriptorValidationException) {
+          return;
+        }
+      }
+      throw e;
     } catch (IOException e) {
       throw new RuntimeException(e);
     }
@@ -103,8 +106,7 @@ class FuzzTestCase {
   private static DynamicMessage getDefaultInstance() {
     try {
       return DynamicMessage.getDefaultInstance(
-              FileDescriptor.buildFrom(file, new FileDescriptor[0])
-                      .findMessageTypeByName(protoName));
+          FileDescriptor.buildFrom(file, new FileDescriptor[0]).findMessageTypeByName(protoName));
     } catch (Descriptors.DescriptorValidationException e) {
       throw new IllegalStateException(e);
     }
