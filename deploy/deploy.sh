148b0c916bac4103142e5fefd644c2073a2d14fb
Robert Czechowski <robert@code-intelligence.com>
2023-08-24 14:28:42 +0200

    blaaa

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -22,8 +22,8 @@ fail() {
 
 cd "$BUILD_WORKSPACE_DIRECTORY" || fail "BUILD_WORKSPACE_DIRECTORY not found"
 
-echo "$RELEASE_KEY_PRIVATE" | gpg --import
-echo "default-key $RELEASE_KEY_ID" > $HOME/.gnupg/gpg.conf
+echo "$RELEASE_SIGNING_KEY_PRIVATE" | gpg --import
+echo "default-key $RELEASE_SIGNING_KEY_ID" > $HOME/.gnupg/gpg.conf
 
 JAZZER_COORDINATES=$1
 
