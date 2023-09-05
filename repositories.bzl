CodeIntelligenceTesting/jazzer

commit 1ca007d04325014d4fa0e48d239745f3ecc8fbcf

BREAKING: junit: Add support for BeforeEach and AfterEach methods
During fuzzing, methods annotated with `@BeforeEach` or `@AfterEach` as
well as implementations of `BeforeEachCallback` and `AfterEachCallback`
are now executed between individual fuzz test executions rather than
only once before and after fuzzing. This more closely matches the
behavior of JUnit unit tests as well as fuzz tests in regression test
mode.

src/main/java/com/code_intelligence/jazzer/junit/BUILD.bazel
@@ -66,6 +66,7 @@ java_jni_library(
     ],
     deps = [
         ":agent_configurator",
+        ":junit_lifecycle_methods_invoker",
         ":seed_serializer",
         ":utils",
         "//src/main/java/com/code_intelligence/jazzer/agent:agent_installer",
@@ -73,7 +74,6 @@ java_jni_library(
         "//src/main/java/com/code_intelligence/jazzer/autofuzz",
         "//src/main/java/com/code_intelligence/jazzer/driver:fuzz_target_holder",
         "//src/main/java/com/code_intelligence/jazzer/driver:fuzz_target_runner",
-        "//src/main/java/com/code_intelligence/jazzer/driver:lifecycle_methods_invoker",
         "//src/main/java/com/code_intelligence/jazzer/driver:opt",
         "//src/main/java/com/code_intelligence/jazzer/driver/junit:exit_code_exception",
         "//src/main/java/com/code_intelligence/jazzer/mutation",
@@ -81,6 +81,25 @@ java_jni_library(
         "@maven//:org_junit_jupiter_junit_jupiter_api",
         "@maven//:org_junit_jupiter_junit_jupiter_params",
         "@maven//:org_junit_platform_junit_platform_commons",
+    ],
+)
+
+java_library(
+    name = "junit_internals_compile_only",
+    neverlink = True,
+    exports = [
+        "@maven//:org_junit_jupiter_junit_jupiter_engine",
+    ],
+)
+
+java_library(
+    name = "junit_lifecycle_methods_invoker",
+    srcs = ["JUnitLifecycleMethodsInvoker.java"],
+    deps = [
+        ":junit_internals_compile_only",
+        "//src/main/java/com/code_intelligence/jazzer/driver:lifecycle_methods_invoker",
+        "//src/main/java/com/code_intelligence/jazzer/utils:unsafe_provider",
+        "@maven//:org_junit_jupiter_junit_jupiter_api",
     ],
 )
 

examples/junit/src/test/java/com/example/LifecycleFuzzTest.java
@@ -20,14 +20,11 @@ import static com.google.common.truth.Truth.assertThat;
 import static java.util.Arrays.asList;
 import static java.util.Collections.unmodifiableList;
 
-import com.code_intelligence.jazzer.api.FuzzerSecurityIssueLow;
 import com.code_intelligence.jazzer.junit.FuzzTest;
 import com.example.LifecycleFuzzTest.LifecycleCallbacks1;
 import com.example.LifecycleFuzzTest.LifecycleCallbacks2;
 import com.example.LifecycleFuzzTest.LifecycleCallbacks3;
-import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -49,6 +46,7 @@ import org.junit.jupiter.api.extension.TestInstancePostProcessor;
 @ExtendWith(LifecycleCallbacks3.class)
 class LifecycleFuzzTest {
   private static final ArrayList<String> events = new ArrayList<>();
+  private static final long RUNS = 3;
 
   private boolean beforeEachCalledOnInstance = false;
   private boolean testInstancePostProcessorCalledOnInstance = false;
@@ -81,7 +79,7 @@ class LifecycleFuzzTest {
     throw new AssertionError("This test should not be executed");
   }
 
-  @FuzzTest(maxExecutions = 3)
+  @FuzzTest(maxExecutions = RUNS)
   void lifecycleFuzz(byte[] data) {
     events.add("lifecycleFuzz");
     assertThat(beforeEachCalledOnInstance).isTrue();
@@ -126,11 +124,11 @@ class LifecycleFuzzTest {
       expectedEvents.addAll(expectedAfterEachEvents);
     }
     if (isFuzzingFromJUnit || isFuzzingFromCommandLine) {
-      expectedEvents.addAll(expectedBeforeEachEvents);
-      // TODO: Fuzz tests currently don't run before each and after each methods between fuzz test
-      // invocations.
-      expectedEvents.addAll(Collections.nCopies(3, "lifecycleFuzz"));
-      expectedEvents.addAll(expectedAfterEachEvents);
+      for (int i = 0; i < RUNS; i++) {
+        expectedEvents.addAll(expectedBeforeEachEvents);
+        expectedEvents.add("lifecycleFuzz");
+        expectedEvents.addAll(expectedAfterEachEvents);
+      }
     }
 
     expectedEvents.add("afterAll");

maven.bzl
@@ -21,12 +21,12 @@ JAZZER_JUNIT_COORDINATES = "com.code-intelligence:jazzer-junit:%s" % JAZZER_VERS
 
 # keep sorted
 MAVEN_ARTIFACTS = [
-    "org.junit.jupiter:junit-jupiter-api:5.8.2",
-    "org.junit.jupiter:junit-jupiter-engine:5.8.2",
-    "org.junit.jupiter:junit-jupiter-params:5.8.2",
-    "org.junit.platform:junit-platform-commons:jar:1.8.2",
-    "org.junit.platform:junit-platform-engine:jar:1.8.2",
-    "org.junit.platform:junit-platform-launcher:jar:1.8.2",
+    "org.junit.jupiter:junit-jupiter-api:5.9.0",
+    "org.junit.jupiter:junit-jupiter-engine:5.9.0",
+    "org.junit.jupiter:junit-jupiter-params:5.9.0",
+    "org.junit.platform:junit-platform-commons:jar:1.9.0",
+    "org.junit.platform:junit-platform-engine:jar:1.9.0",
+    "org.junit.platform:junit-platform-launcher:jar:1.9.0",
     "org.opentest4j:opentest4j:1.2.0",
 ]
 

src/main/java/com/code_intelligence/jazzer/driver/LifecycleMethodsInvoker.java
@@ -21,22 +21,18 @@ package com.code_intelligence.jazzer.driver;
  * execution of a fuzz target.
  */
 public interface LifecycleMethodsInvoker {
-
   /**
    * An implementation of {@link LifecycleMethodsInvoker} with empty implementations.
    */
   LifecycleMethodsInvoker NOOP = new LifecycleMethodsInvoker() {
     @Override
-    public void beforeFirstExecution() {
-    }
+    public void beforeFirstExecution() {}
 
     @Override
-    public void beforeEachExecution() {
-    }
+    public void beforeEachExecution() {}
 
     @Override
-    public void afterLastExecution() {
-    }
+    public void afterLastExecution() {}
   };
 
   /**

src/main/java/com/code_intelligence/jazzer/junit/FuzzTestExecutor.java
@@ -23,7 +23,6 @@ import static java.util.stream.Collectors.toList;
 import com.code_intelligence.jazzer.agent.AgentInstaller;
 import com.code_intelligence.jazzer.driver.FuzzTargetHolder;
 import com.code_intelligence.jazzer.driver.FuzzTargetRunner;
-import com.code_intelligence.jazzer.driver.LifecycleMethodsInvoker;
 import com.code_intelligence.jazzer.driver.Opt;
 import com.code_intelligence.jazzer.driver.junit.ExitCodeException;
 import java.io.File;
@@ -304,8 +303,8 @@ class FuzzTestExecutor {
   }
 
   @SuppressWarnings("OptionalGetWithoutIsPresent")
-  public Optional<Throwable> execute(
-      ReflectiveInvocationContext<Method> invocationContext, SeedSerializer seedSerializer) {
+  public Optional<Throwable> execute(ReflectiveInvocationContext<Method> invocationContext,
+      ExtensionContext extensionContext, SeedSerializer seedSerializer) {
     if (seedSerializer instanceof AutofuzzSeedSerializer) {
       FuzzTargetHolder.fuzzTarget = FuzzTargetHolder.autofuzzFuzzTarget(() -> {
         // Provide an empty throws declaration to prevent autofuzz from
@@ -323,7 +322,9 @@ class FuzzTestExecutor {
     } else {
       FuzzTargetHolder.fuzzTarget =
           new FuzzTargetHolder.FuzzTarget(invocationContext.getExecutable(),
-              () -> invocationContext.getTarget().get(), LifecycleMethodsInvoker.NOOP);
+              ()
+                  -> invocationContext.getTarget().get(),
+              JUnitLifecycleMethodsInvoker.of(extensionContext));
     }
 
     // Only register a finding handler in case the fuzz test is executed by JUnit.

src/main/java/com/code_intelligence/jazzer/junit/FuzzTestExtensions.java
@@ -109,9 +109,9 @@ class FuzzTestExtensions
       ReflectiveInvocationContext<Method> invocationContext, ExtensionContext extensionContext)
       throws Throwable {
     invocation.skip();
-    Optional<Throwable> throwable =
-        FuzzTestExecutor.fromContext(extensionContext)
-            .execute(invocationContext, getOrCreateSeedSerializer(extensionContext));
+    Optional<Throwable> throwable = FuzzTestExecutor.fromContext(extensionContext)
+                                        .execute(invocationContext, extensionContext,
+                                            getOrCreateSeedSerializer(extensionContext));
     if (throwable.isPresent()) {
       throw throwable.get();
     }

src/main/java/com/code_intelligence/jazzer/junit/JUnitLifecycleMethodsInvoker.java
@@ -1,0 +1,137 @@
+/*
+ * Copyright 2023 Code Intelligence GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.code_intelligence.jazzer.junit;
+
+import static java.util.stream.Collectors.toCollection;
+
+import com.code_intelligence.jazzer.driver.LifecycleMethodsInvoker;
+import com.code_intelligence.jazzer.utils.UnsafeProvider;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Optional;
+import java.util.stream.Stream;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.engine.execution.AfterEachMethodAdapter;
+import org.junit.jupiter.engine.execution.BeforeEachMethodAdapter;
+import org.junit.jupiter.engine.execution.DefaultExecutableInvoker;
+import org.junit.jupiter.engine.extension.ExtensionRegistry;
+
+/**
+ * Adapts JUnit BeforeEach and AfterEach callbacks to
+ * {@link com.code_intelligence.jazzer.driver.FuzzTargetRunner} lifecycle hooks.
+ */
+public class JUnitLifecycleMethodsInvoker implements LifecycleMethodsInvoker {
+  private final ThrowingRunnable[] beforeEachExecutionRunnables;
+
+  private long timesCalledBetweenExecutions = 0;
+
+  private JUnitLifecycleMethodsInvoker(ThrowingRunnable[] beforeEachExecutionRunnables) {
+    this.beforeEachExecutionRunnables = beforeEachExecutionRunnables;
+  }
+
+  static LifecycleMethodsInvoker of(ExtensionContext extensionContext) {
+    // ExtensionRegistry is private JUnit API that is the source of truth for all lifecycle
+    // callbacks, both annotation- and extension-based.
+    Optional<ExtensionRegistry> maybeExtensionRegistry =
+        getExtensionRegistryViaHack(extensionContext);
+    if (!maybeExtensionRegistry.isPresent()) {
+      extensionContext.publishReportEntry(
+          "Jazzer does not support BeforeEach and AfterEach callbacks with this version of JUnit.");
+      return LifecycleMethodsInvoker.NOOP;
+    }
+    ExtensionRegistry extensionRegistry = maybeExtensionRegistry.get();
+
+    // BeforeEachCallback implementations take precedence over @BeforeEach methods. The annotations
+    // are turned into extensions using an internal adapter class, BeforeEachMethodAdapter.
+    // https://junit.org/junit5/docs/current/user-guide/#extensions-execution-order-wrapping-behavior
+    ArrayList<ThrowingRunnable> beforeEachMethods =
+        Stream
+            .<ThrowingRunnable>concat(
+                extensionRegistry.stream(BeforeEachCallback.class)
+                    .map(callback -> () -> callback.beforeEach(extensionContext)),
+                extensionRegistry.stream(BeforeEachMethodAdapter.class)
+                    .map(callback
+                        -> ()
+                            -> callback.invokeBeforeEachMethod(
+                                extensionContext, extensionRegistry)))
+            .collect(toCollection(ArrayList::new));
+
+    ArrayList<ThrowingRunnable> afterEachMethods =
+        Stream
+            .<ThrowingRunnable>concat(
+                extensionRegistry.stream(AfterEachCallback.class)
+                    .map(callback -> () -> callback.afterEach(extensionContext)),
+                extensionRegistry.stream(AfterEachMethodAdapter.class)
+                    .map(callback
+                        -> ()
+                            -> callback.invokeAfterEachMethod(extensionContext, extensionRegistry)))
+            .collect(toCollection(ArrayList::new));
+    // JUnit calls AfterEach methods in reverse order of registration so that the methods registered
+    // first run last.
+    Collections.reverse(afterEachMethods);
+
+    return new JUnitLifecycleMethodsInvoker(
+        Stream.concat(afterEachMethods.stream(), beforeEachMethods.stream())
+            .toArray(ThrowingRunnable[] ::new));
+  }
+
+  private static Optional<ExtensionRegistry> getExtensionRegistryViaHack(
+      ExtensionContext extensionContext) {
+    // Do not fail on JUnit versions < 5.9.0 that do not have DefaultExecutableInvoker.
+    try {
+      Class.forName("org.junit.jupiter.engine.execution.DefaultExecutableInvoker");
+    } catch (ClassNotFoundException e) {
+      return Optional.empty();
+    }
+    // Get the private DefaultExecutableInvoker#extensionRegistry field, using the type rather than
+    // the name for slightly better forwards compatibility.
+    return Arrays.stream(DefaultExecutableInvoker.class.getDeclaredFields())
+        .filter(field -> field.getType() == ExtensionRegistry.class)
+        .findFirst()
+        .flatMap(extensionRegistryField -> {
+          DefaultExecutableInvoker invoker =
+              (DefaultExecutableInvoker) extensionContext.getExecutableInvoker();
+          long extensionRegistryFieldOffset =
+              UnsafeProvider.getUnsafe().objectFieldOffset(extensionRegistryField);
+          return Optional.ofNullable((ExtensionRegistry) UnsafeProvider.getUnsafe().getObject(
+              invoker, extensionRegistryFieldOffset));
+        });
+  }
+
+  @Override
+  public void beforeFirstExecution() {}
+
+  @Override
+  public void beforeEachExecution() throws Throwable {
+    if (timesCalledBetweenExecutions++ == 0) {
+      // BeforeEach callbacks are run by JUnit right before the fuzz test starts executing and thus
+      // shouldn't be run again before the first fuzz test execution.
+      // AfterEach callbacks should be run between two executions and thus also not before the first
+      // fuzz test execution.
+      return;
+    }
+    for (ThrowingRunnable runnable : beforeEachExecutionRunnables) {
+      runnable.run();
+    }
+  }
+
+  @Override
+  public void afterLastExecution() {}
+}

commit 1bc151082abf2a6e8f27b5cd7e6413dc5b2b6a96

junit: Do not depend on `junit-jupiter-engine`
The user has to add and control this dependency themselves if they want
to use JUnit 5. Even if we added it, Maven resolution mechanics mean
that we do not control the version anyway.

This fixes an issue introduced by 1ca007d04325014d4fa0e48d239745f3ecc8fbcf.

See https://github.com/junit-team/junit5/discussions/3441#discussioncomment-6884855

Also require fixing an issue in rules_jvm_external that results in
neverlink deps not appearing on the javadoc classpath.

repositories.bzl
@@ -64,6 +64,9 @@ def jazzer_dependencies(android = False):
             # Fixes an incompatibility with latest Bazel introduced in
             # https://github.com/bazelbuild/bazel/commit/d0e29582a2e788e8acdaf53fe30ab7f7dc592df3
             "//third_party:rules_jvm_external-add-toolchain-to-maven-project-jar.patch",
+            # https://github.com/bazelbuild/rules_jvm_external/pull/952
+            # Fixes javadoc generation when using neverlink dependencies.
+            "//third_party:rules_jvm_external-add-neverlink-deps-to-javadoc-classpath.patch",
         ],
         sha256 = "d31e369b854322ca5098ea12c69d7175ded971435e55c18dd9dd5f29cc5249ac",
         strip_prefix = "rules_jvm_external-5.3",

src/main/java/com/code_intelligence/jazzer/junit/BUILD.bazel
@@ -87,6 +87,12 @@ java_jni_library(
 java_library(
     name = "junit_internals_compile_only",
     neverlink = True,
+    # Do not add a dependency on junit-jupiter-engine to the POM file. The user
+    # has to add and control this dependency themselves if they want to use
+    # JUnit 5. Even if we added it, Maven resolution mechanics mean that we do
+    # not control the version anyway.
+    # https://github.com/junit-team/junit5/discussions/3441#discussioncomment-6884855
+    tags = ["maven:compile-only"],
     exports = [
         "@maven//:org_junit_jupiter_junit_jupiter_engine",
     ],

third_party/rules_jvm_external-add-neverlink-deps-to-javadoc-classpath.patch
@@ -1,0 +1,33 @@
+From 920048a2b213e2c7cb6ce679ffa5a414054339f6 Mon Sep 17 00:00:00 2001
+From: Fabian Meumertzheim <fabian@meumertzhe.im>
+Date: Fri, 1 Sep 2023 22:12:42 +0200
+Subject: [PATCH] Add compile-only deps to javadocs classpath
+
+javadoc may have to inspect compile-only dependencies.
+
+Also removes a line that only added elements to a depset that are
+already contained in this depset.
+---
+ private/rules/javadoc.bzl | 9 ++++++---
+ 1 file changed, 6 insertions(+), 3 deletions(-)
+
+diff --git a/private/rules/javadoc.bzl b/private/rules/javadoc.bzl
+index 325aced1..3261248a 100644
+--- a/private/rules/javadoc.bzl
++++ b/private/rules/javadoc.bzl
+@@ -21,9 +21,12 @@ def _javadoc_impl(ctx):
+ 
+     jar_file = ctx.actions.declare_file("%s.jar" % ctx.attr.name)
+ 
+-    # Gather additional files to add to the classpath
+-    additional_deps = depset(transitive = [dep[JavaInfo].transitive_runtime_jars for dep in ctx.attr.deps])
+-    classpath = depset(transitive = [dep[JavaInfo].transitive_runtime_jars for dep in ctx.attr.deps] + [additional_deps])
++    # javadoc may need to inspect compile-time dependencies (neverlink)
++    # of the runtime classpath.
++    classpath = depset(
++        transitive = [dep[JavaInfo].transitive_runtime_jars for dep in ctx.attr.deps] +
++                     [dep[JavaInfo].transitive_compile_time_jars for dep in ctx.attr.deps],
++    )
+ 
+     # javadoc options and javac options overlap, but we cannot
+     # necessarily rely on those to derive the javadoc options we need