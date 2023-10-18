5dd8a0268e795b1f585754b901c1e8d7dab2e51c
Fabian Meumertzheim <meumertzheim@code-intelligence.com>
2023-10-18 14:23:26 +0200

    BREAKING: junit: Use relative resource paths with `@DictionaryFile`
    The resource path is now interpreted relative to the class using the
    annotation, which is more idiomatic. This does require changing
    `com/example/Foo` to `/com/example/Foo` if an absolute path is desired.

--- src/main/java/com/code_intelligence/jazzer/junit/DictionaryFile.java
+++ src/main/java/com/code_intelligence/jazzer/junit/DictionaryFile.java
@@ -23,13 +23,13 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 /**
- * Defines a reference to a dictionary within the resources directory. The given {@code
- * resourcePath} must be an absolute path and must not have a leading {@code /}. These should follow
- * <a href="https://llvm.org/docs/LibFuzzer.html#dictionaries">libfuzzer's dictionary syntax</a>.
+ * Attaches a dictionary file located in resources to a fuzz test. The file should follow <a
+ * href="https://llvm.org/docs/LibFuzzer.html#dictionaries">libfuzzer's dictionary syntax</a>.
  */
 @Target({ElementType.METHOD, ElementType.ANNOTATION_TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Repeatable(DictionaryFiles.class)
 public @interface DictionaryFile {
+  /** The resource path of the dictionary file. */
   String resourcePath();
 }
