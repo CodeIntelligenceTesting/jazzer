3aeeec04f1250b9261dac02a1739fcc07571cbfb
Norbert Schneider <norbert.schneider@code-intelligence.com>
2024-03-26 11:04:05 +0100

    chore: move exit code exception into junit module

--- src/main/java/com/code_intelligence/jazzer/junit/ExitCodeException.java
+++ src/main/java/com/code_intelligence/jazzer/junit/ExitCodeException.java
@@ -7,7 +7,7 @@
  * located in the root directory of the project.
  */
 
-package com.code_intelligence.jazzer.driver.junit;
+package com.code_intelligence.jazzer.junit;
 
 public final class ExitCodeException extends Exception {
   public final int exitCode;
