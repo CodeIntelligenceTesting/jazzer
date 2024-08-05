4621ea243e07aadae3491bae7a883469bf4d0030
Khaled Yakdan <yakdan@code-intelligence.com>
2024-08-05 22:26:38 +0200

    chore: change source files headers to mention the new license

--- tests/src/test/cc/complex_proto_fuzzer.cc
+++ tests/src/test/cc/complex_proto_fuzzer.cc
@@ -1,16 +1,9 @@
-// Copyright 2023 Code Intelligence GmbH
+// Copyright 2024 Code Intelligence GmbH
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// By downloading, you agree to the Code Intelligence Jazzer Terms and Conditions.
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+// The Code Intelligence Jazzer Terms and Conditions are provided in LICENSE-JAZZER.txt
+// located in the root directory of the project.
 
 #include "src/libfuzzer/libfuzzer_macro.h"
 #include "src/test/java/com/code_intelligence/jazzer/mutation/mutator/proto/proto2.pb.h"
