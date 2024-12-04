3407991d2875c4359c4332c5623d7332fc114a79
Khaled Yakdan <yakdan@code-intelligence.com>
2024-12-04 18:09:52 +0100

    chore: update copyright headers to mention Apache 2.0

--- src/test/java/com/code_intelligence/jazzer/mutation/mutator/lang/NullableMutatorTest.java
+++ src/test/java/com/code_intelligence/jazzer/mutation/mutator/lang/NullableMutatorTest.java
@@ -1,10 +1,17 @@
 /*
  * Copyright 2024 Code Intelligence GmbH
  *
- * By downloading, you agree to the Code Intelligence Jazzer Terms and Conditions.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * The Code Intelligence Jazzer Terms and Conditions are provided in LICENSE-JAZZER.txt
- * located in the root directory of the project.
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package com.code_intelligence.jazzer.mutation.mutator.lang;
