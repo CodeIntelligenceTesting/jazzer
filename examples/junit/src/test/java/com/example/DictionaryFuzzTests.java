b68be37dc8f55a2657e68459b8cdea9ef7560ce9
Peter Samarin <peter.samarin@code-intelligence.com>
2025-08-04 23:57:31 +0200

    chore(tests): speed up dictionary tests
    The dictionary tests make sure that the annotations @DictionaryEntries
    and @DictionaryFile actually get used in the fuzz test.
    However, for these tests, the fuzzer runs for a long time on
    Windows. Here are the test runtime changes from before and after this commit:
    - DictionaryFuzzTests_fileTest - 200.8s => 14.2s
    - DictionaryFuzzTests_inlineTest 124.4s => 11.7s
    - DictionaryFuzzTests_mixedTest 87.8s => 16.9s

--- examples/junit/src/test/java/com/example/DictionaryFuzzTests.java
+++ examples/junit/src/test/java/com/example/DictionaryFuzzTests.java
@@ -32,7 +32,7 @@ public class DictionaryFuzzTests {
   private static final byte[] FLAG_SHA256 =
       Base64.getDecoder().decode("vCLInoVuMxJonT4UKjsMl0LPXTowkYS7t0uBpw0pRo8=");
 
-  @DictionaryEntries({"a_", "53Cr\"3T_", "fl4G"})
+  @DictionaryEntries({"a_53Cr\"3T_fl4G"})
   @FuzzTest
   public void inlineTest(FuzzedDataProvider data)
       throws NoSuchAlgorithmException, TestSuccessfulException {
@@ -54,7 +54,27 @@ public class DictionaryFuzzTests {
     }
   }
 
-  @DictionaryEntries("a_")
+  /**
+   * This test uses multiple dictionaries, one of which is loaded on-the-fly. The other two are
+   * loaded from files. The test will only succeed if entries from all three dictionaries have been
+   * used at least once to generate fuzzer data input completely.
+   */
+  private static final byte[] ON_THE_FLY_FLAG_SHA256 =
+      Base64.getDecoder().decode("YgF0VLwI07ls++qMJ/Ptmc9Q7xlIErMXXXK1enpNqKw=");
+
+  private static final byte[] FILE_TEST2_FLAG_SHA256 =
+      Base64.getDecoder().decode("nIQ780j1iRJbOouWMvwhmS63i5lcHlhBf780CFmXvdc=");
+  private static final byte[] FILE_TEST3_FLAG_SHA256 =
+      Base64.getDecoder().decode("JR9EuaWZ5hyEHy2Ynfh1KCLI60UL8evAYYTAoJhCcaY=");
+
+  private static boolean onTheFlyDictionaryFound =
+      false; // =o1pPß?1bHJAfas => YgF0VLwI07ls++qMJ/Ptmc9Q7xlIErMXXXK1enpNqKw=
+  private static boolean fileTest2DictFound =
+      false; // 0Z1o21ka => nIQ780j1iRJbOouWMvwhmS63i5lcHlhBf780CFmXvdc=
+  private static boolean fileTest3DictFound =
+      false; // A)716&=Ko => JR9EuaWZ5hyEHy2Ynfh1KCLI60UL8evAYYTAoJhCcaY=
+
+  @DictionaryEntries("=o1pPß?1bHJAfas")
   @DictionaryFile(resourcePath = "test2.dict")
   @DictionaryFile(resourcePath = "/com/example/test3.dict")
   @FuzzTest
@@ -62,7 +82,14 @@ public class DictionaryFuzzTests {
       throws NoSuchAlgorithmException, TestSuccessfulException {
     String s = data.consumeRemainingAsString();
     byte[] hash = MessageDigest.getInstance("SHA-256").digest(s.getBytes(StandardCharsets.UTF_8));
-    if (MessageDigest.isEqual(hash, FLAG_SHA256)) {
+    if (MessageDigest.isEqual(hash, ON_THE_FLY_FLAG_SHA256)) {
+      onTheFlyDictionaryFound = true;
+    } else if (MessageDigest.isEqual(hash, FILE_TEST2_FLAG_SHA256)) {
+      fileTest2DictFound = true;
+    } else if (MessageDigest.isEqual(hash, FILE_TEST3_FLAG_SHA256)) {
+      fileTest3DictFound = true;
+    }
+    if (onTheFlyDictionaryFound && fileTest2DictFound && fileTest3DictFound) {
       throw new TestSuccessfulException("error found");
     }
   }
