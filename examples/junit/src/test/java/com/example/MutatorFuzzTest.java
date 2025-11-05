679d035d550399d34467a4d4c0db4eaf47273d27
Florian Gläßer <florian.glaesser@code-intelligence.com>
2025-11-05 15:57:35 +0100

    test

--- examples/junit/src/test/java/com/example/MutatorFuzzTest.java
+++ examples/junit/src/test/java/com/example/MutatorFuzzTest.java
@@ -20,16 +20,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.code_intelligence.jazzer.driver.FuzzTargetRunner;
 import com.code_intelligence.jazzer.junit.FuzzTest;
-import com.code_intelligence.jazzer.mutation.annotation.NotNull;
-import java.util.List;
 import org.junit.jupiter.api.AfterAll;
 
 class MutatorFuzzTest {
   @FuzzTest
-  void mutatorFuzz(List<@NotNull String> list) {
-    // Check that the mutator is actually doing something.
-    if (list != null && list.size() > 3 && list.get(2).equals("mutator")) {
-      throw new AssertionError("Found expected JUnit mutator test issue");
+  void mutatorFuzz(char[] data) {
+    if (data == null) {
+      return;
+    }
+    String expression = new String(data);
+    if (expression.contains("jazzer")) {
+      throw new RuntimeException("found jazzer");
     }
   }
 
